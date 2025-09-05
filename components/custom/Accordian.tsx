2f395efa4ab7eca01ac7d4479c48b4182aa8c1c8
AbdulMoeez231 <moeezbuttab231@gmail.com>
2025-09-05 17:56:34 +0500

    Add collapsed prop to Accordian component and update SideBar integration

--- components/custom/Accordian.tsx
+++ components/custom/Accordian.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Link from "next/link";
 import {
   Accordion,
@@ -5,6 +6,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { useRouter } from "next/navigation";
 
 // --- RouteItem Type ---
 type RouteItem = {
@@ -12,8 +14,9 @@ type RouteItem = {
   label: string;
 };
 
-const Accordian = ({ items, label, icon }: { items: RouteItem[], label: string, icon: any }) => {
+const Accordian = ({ items, label, icon, collapsed }: { items: RouteItem[], label: string, icon: any, collapsed: Boolean }) => {
   const Icon = icon
+  const pathname = window?.location?.pathname;
   return (
     <div>
       <Accordion type="single" collapsible>
@@ -25,26 +28,29 @@ const Accordian = ({ items, label, icon }: { items: RouteItem[], label: string, 
               <span>
                 <Icon className="shrink-0 size-6" color="#737373" />
               </span>
-              <span className="text-[#737373] font-medium text-[14px]">
+              <span className="text-[#404040] font-medium text-[14px]">
                 {label}
               </span>
             </div>
           </AccordionTrigger>
           <AccordionContent>
             <ul className="flex flex-col gap-2">
-              {items.map((child) => (
-                <li
-                  key={child.href}
-                  className="py-[8px] px-[10px] rounded-lg gap-[12px] hover:bg-[#F5F5F5] cursor-pointer"
-                >
-                  <Link
-                    href={child.href}
-                    className="text-[#404040] font-outfit font-medium text-sm hover:text-blue-600"
-                  >
-                    {child.label}
-                  </Link>
-                </li>
-              ))}
+              {items.map((child) => {
+                const isActive = pathname.includes(child.href);
+                return (
+                  <li
+                    key={child.href}
+                    className="rounded-lg gap-[12px] hover:bg-[#F5F5F5] cursor-pointer">
+                    <Link
+                      href={child.href}
+                      className={`text-[#404040] font-outfit font-medium text-sm hover:text-primary ${isActive ? 'text-primary' : ''}`}>
+                      <p className="py-[8px] px-[10px]">
+                        {child.label}
+                      </p>
+                    </Link>
+                  </li>
+                )
+              })}
             </ul>
           </AccordionContent>
         </AccordionItem>
