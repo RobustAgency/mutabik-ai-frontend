48047fc7531ea21ff146eca49e4202b36bdc1678
AbdulMoeez231 <moeezbuttab231@gmail.com>
2025-09-11 16:54:54 +0500

    fix: update compatibility date in wrangler.toml and adjust script order in package.json

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,6 @@
 import type { NextConfig } from "next";
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
@@ -8,5 +10,10 @@ const nextConfig: NextConfig = {
     ],
   },
 };
+
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
