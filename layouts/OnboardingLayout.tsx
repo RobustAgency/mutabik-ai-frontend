e7dde66e093fc4ac7c1ef0aad823266c27ab4702
AbdulMoeez231 <moeezbuttab231@gmail.com>
2025-09-12 15:31:38 +0500

    fix: adjust header layout and improve logout button visibility in OnboardingLayout

--- layouts/OnboardingLayout.tsx
+++ layouts/OnboardingLayout.tsx
@@ -69,8 +69,17 @@ const OnboardingLayout = ({ children }: { children: React.ReactNode }) => {
         </div>
       </aside>
       <div className="flex-1 w-full md:ml-[260px] flex flex-col min-h-screen">
-        <header className="w-full fixed top-0 left-0  z-20 bg-white flex flex-col md:flex-row md:justify-end items-center h-auto md:h-[100px] px-4 py-4 gap-4">
+        <header className="w-full fixed top-0 left-0  z-20 bg-white flex flex-row justify-end items-center h-[70px] md:h-[100px] px-4 py-4 gap-4">
           {/* Horizontal stepper for mobile only */}
+          {/* <div className="flex w-full justify-center md:hidden mb-2">
+            <Stepper
+              steps={steps}
+              activeStep={activeStep}
+              orientation="horizontal"
+
+              className="w-full max-w-md"
+            />
+          </div> */}
           <Button
             onClick={() => {
               router.push("/logout")
@@ -79,11 +88,11 @@ const OnboardingLayout = ({ children }: { children: React.ReactNode }) => {
             className="flex items-center py-6 px-6 border border-[#D0D5DD] gap-2 text-[#0B2232] hover:bg-[#F1F5F9] transition-colors shadow-none rounded-lg text-base font-medium"
           >
             <LogOut className=" text-[#0B2232]" />
-            <span className="hidden sm:inline">Logout</span>
+            <span>Logout</span>
           </Button>
         </header>
         {/* Main content area */}
-        <main className="flex-1 w-full bg-white pt-[150px] px-2 md:px-20 lg:px-36 pb-8">
+        <main className="flex-1 w-full bg-white pt-[150px] px-2 md:px-20 lg:px-36 pb-8 max-md:ml-5">
           {children}
         </main>
       </div>
