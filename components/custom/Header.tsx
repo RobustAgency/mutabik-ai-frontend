97fe1161d0da7abb1baf02aad92d46f3dc995675
MalikAhsan123 <130451218+MalikAhsan123@users.noreply.github.com>
2025-10-07 17:01:28 +0500

    fix header and add member page feedbacks

--- components/custom/Header.tsx
+++ components/custom/Header.tsx
@@ -1,49 +1,78 @@
-import { cn } from '@/lib/utils'
-import { Button } from '../ui/button'
-import { ChevronLeft } from 'lucide-react'
-import { DrawerTrigger } from '../ui/drawer'
-import ProfileInfo from './ProfileInfo'
-import SearchBar from './SearchBar'
-import Image from 'next/image'
-import Link from 'next/link'
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
+import { ChevronLeft, Divide } from "lucide-react";
+import { DrawerTrigger } from "../ui/drawer";
+import ProfileInfo from "./ProfileInfo";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter, useSearchParams } from "next/navigation";
+import { ArrowLeft } from "lucide-react";
 
 const Header = () => {
-    const headerGridClass = cn(
-        "grid w-full min-h-14 sticky top-0 z-30 border-b bg-background/80 backdrop-blur py-1 bg-[#FFFFFF]",
-        // Mobile: content + profile columns
-        "grid-cols-[minmax(0,1fr)_auto]",
-        // Desktop: first column reserved for sidebar width
-        "md:grid-cols-[200px_minmax(0,1fr)]"
-    )
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const step = searchParams.get("step");
+  console.log("params", step);
+  const headerGridClass = cn(
+    "grid w-full min-h-14 sticky top-1.5 z-30 bg-background/80 backdrop-blur py-1  bg-[#FAFAFA]",
+    // Mobile: content + profile columns
+    "grid-cols-[minmax(0,1fr)_auto]",
+    // Desktop: first column reserved for sidebar width
+    "md:grid-cols-[300px_minmax(0,1fr)]"
+  );
 
-    return (
-        <header className={headerGridClass}>
-            <div className="h-full w-full flex items-center justify-between px-0 xs:px-4">
-                <div className="w-full flex items-center justify-start md:justify-between gap-0 md:gap-2">
-                    <div aria-details="logo" className='pl-2'>
-                        <Link href="/" className='hidden md:block'>
-                            <Image src="/auth/dashboard-logo.svg" alt="logo" width={120} height={56} />
-                        </Link>
-                    </div>
-                    <DrawerTrigger className="md:hidden" asChild>
-                        <Button
-                            variant="outline"
-                            className="size-8"
-                            size="icon"
-                            aria-label="Open sidebar"
-                        >
-                            <ChevronLeft className="size-4 rotate-180" />
-                        </Button>
-                    </DrawerTrigger>
-                </div>
+  return (
+    <header className={headerGridClass}>
+      <div className="h-full w-full flex items-center justify-between px-0 xs:px-4">
+        <div className="w-full flex items-center justify-start md:justify-between gap-0 md:gap-2">
+          <div aria-details="logo" className="pl-2">
+            <Link href="/" className="hidden md:block">
+              <Image
+                src="/auth/dashboard-logo.svg"
+                alt="logo"
+                width={120}
+                height={56}
+              />
+            </Link>
+          </div>
+          <DrawerTrigger className="md:hidden" asChild>
+            <Button
+              variant="outline"
+              className="size-8"
+              size="icon"
+              aria-label="Open sidebar"
+            >
+              <ChevronLeft className="size-4 rotate-180" />
+            </Button>
+          </DrawerTrigger>
+        </div>
+      </div>
+      <div className="flex items-center justify-between  gap-0.5">
+        {!step && (
+          <p
+            className="font-semibold text-[20px]  text-[#1E1E1E]"
+          >
+            Projects
+          </p>
+        )}
+        {step && (
+          <div className="flex justify-center items-center gap-4 ">
+            <div onClick={() => {
+              router.push("/projects");
+            }} className="w-11 h-11 cursor-pointer flex justify-center items-center gap-[10px] opacity-100 p-1 border border-[#E4E7EC] rounded-lg">
+              <ArrowLeft color="#757575" className="w-4 h-4 opacity-100" />
             </div>
-            <div className='flex items-center justify-end gap-0.5'>
-                {/* <SearchBar /> */}
-                <ProfileInfo />
-            </div>
+            <p onClick={() => {
+              router.push("/projects");
+            }} className="font-normal text-base text-[#757575] cursor-pointer">Back to Projects</p>
+          </div>
+        )}
 
-        </header>
-    )
-}
+        {/* <SearchBar /> */}
+        <ProfileInfo />
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- components/custom/ProfileInfo.tsx
+++ components/custom/ProfileInfo.tsx
@@ -1,51 +1,112 @@
-import { useAuth } from '@/providers/AuthProvider'
-import Image from 'next/image'
-import React, { useEffect } from 'react'
+// import { useAuth } from '@/providers/AuthProvider'
+// import Image from 'next/image'
+// import React, { useEffect } from 'react'
+// import {
+//     DropdownMenu,
+//     DropdownMenuTrigger,
+//     DropdownMenuContent,
+//     DropdownMenuItem,
+// } from '@/components/ui/dropdown-menu'
+// import { useRouter } from 'next/navigation'
+
+// const ProfileInfo = () => {
+//     const { profile, fetchProfile, } = useAuth();
+//     const avatarUrl = profile?.avatar_url || "/profile-logo.png"
+//     // const avatarUrl = profile?.avatar_url
+//     // const displayName = profile?.full_name ?? "User"
+//     // const role = user?.user_metadata?.role ?? "user"
+//     useEffect(() => {
+//         if (!profile) {
+//             fetchProfile()
+//         }
+//     }, [profile, fetchProfile])
+
+//     const router = useRouter();
+//     return (
+//         <div className="flex items-center gap-3 justify-end px-4">
+//             <DropdownMenu>
+//                 <DropdownMenuTrigger asChild>
+//                     <button
+//                         className="relative h-10 w-10 overflow-hidden rounded-full bg-muted focus:outline-none "
+//                         aria-label="Open profile menu"
+//                     >
+//                         {avatarUrl && (
+//                             <Image src={avatarUrl} alt="Profile" fill sizes="40px" className="object-cover cursor-pointer" unoptimized />
+//                         )}
+//                     </button>
+                    
+//                 </DropdownMenuTrigger>
+//                 <DropdownMenuContent align="end" className="w-40 ">
+//                     <DropdownMenuItem className='cursor-pointer' onClick={() => router.push('/settings')}>
+//                         Settings
+//                     </DropdownMenuItem>
+//                     <DropdownMenuItem className='cursor-pointer' onClick={() => router.push('/logout')}>
+//                         Logout
+//                     </DropdownMenuItem>
+//                 </DropdownMenuContent>
+//             </DropdownMenu>
+//         </div>
+//     )
+// }
+
+// export default ProfileInfo
+"use client";
+
+import { useAuth } from "@/providers/AuthProvider";
+import Image from "next/image";
+import React, { useEffect } from "react";
 import {
-    DropdownMenu,
-    DropdownMenuTrigger,
-    DropdownMenuContent,
-    DropdownMenuItem,
-} from '@/components/ui/dropdown-menu'
-import { useRouter } from 'next/navigation'
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { useRouter } from "next/navigation";
+import { ChevronDown } from "lucide-react"; // ✅ Import dropdown icon
 
 const ProfileInfo = () => {
-    const { profile, fetchProfile, } = useAuth();
-    const avatarUrl = profile?.avatar_url || "/placeholders/user_placeholder.png"
-    // const avatarUrl = profile?.avatar_url
-    // const displayName = profile?.full_name ?? "User"
-    // const role = user?.user_metadata?.role ?? "user"
-    useEffect(() => {
-        if (!profile) {
-            fetchProfile()
-        }
-    }, [profile, fetchProfile])
+  const { profile, fetchProfile } = useAuth();
+  const avatarUrl = profile?.avatar_url || "/profile-logo.png";
 
-    const router = useRouter();
-    return (
-        <div className="flex items-center gap-3 justify-end px-4">
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <button
-                        className="relative h-10 w-10 overflow-hidden rounded-full bg-muted focus:outline-none "
-                        aria-label="Open profile menu"
-                    >
-                        {avatarUrl && (
-                            <Image src={avatarUrl} alt="Profile" fill sizes="40px" className="object-cover cursor-pointer" unoptimized />
-                        )}
-                    </button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end" className="w-40 ">
-                    <DropdownMenuItem className='cursor-pointer' onClick={() => router.push('/settings')}>
-                        Settings
-                    </DropdownMenuItem>
-                    <DropdownMenuItem className='cursor-pointer' onClick={() => router.push('/logout')}>
-                        Logout
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
+  useEffect(() => {
+    if (!profile) {
+      fetchProfile();
+    }
+  }, [profile, fetchProfile]);
+
+  const router = useRouter();
+
+  return (
+    <div className="flex items-center gap-3 justify-end px-4">
+        <div>
+<Image src={avatarUrl} alt="Profile"  width={44} height={42} className="cursor-pointer" />
         </div>
-    )
-}
+        
+      <DropdownMenu>
+        <DropdownMenuTrigger className="cursor-pointer" asChild>
+          <div className="flex items-center gap-1">
+            <p className=" font-medium text-sm text-[#344054] ">Ai-Noor Holdings</p>
+            <ChevronDown color="#667085" className="w-4  " />
+          </div>
+        </DropdownMenuTrigger>
 
-export default ProfileInfo
+        <DropdownMenuContent align="end" className="w-40">
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onClick={() => router.push("/settings")}
+          >
+            Settings
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onClick={() => router.push("/logout")}
+          >
+            Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default ProfileInfo;
